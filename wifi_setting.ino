15b09192b3a73073e4b4a4353b780d87b0799a24
Tadashi Ogasawara <1bjt2225@gmail.com>
2018-01-20 22:47:37 +0900

    add sample

--- wifi_setting.ino
+++ wifi_setting.ino
@@ -1,16 +1,30 @@
+//
+// 参考Sample
+// ESP8266 (ESP-WROOM-02) でWi-Fi使用時のSSID/パスワードをブラウザで設定できるように
+// https://qiita.com/exabugs/items/2f67ae363a1387c8967c
+//
+
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <FS.h>
 
 
-const int MODE_PIN = 0; // GPIO0// モード切り替えピン
-const char* settings = "/wifi_settings.txt";// Wi-Fi設定保存ファイル
-const String pass = "password";// サーバモードでのパスワード
+// モード切り替えピン
+const int MODE_PIN = 0; // GPIO0
+
+// Wi-Fi設定保存ファイル
+const char* settings = "/wifi_settings.txt";
+
+// サーバモードでのパスワード
+const String pass = "thereisnospoon";
+
 
 ESP8266WebServer server(80);
 
-/*** WiFi設定 ***/
+/**
+ * WiFi設定
+ */
 void handleRootGet() {
   String html = "";
   html += "<h1>WiFi Settings</h1>";
@@ -38,7 +52,9 @@ void handleRootPost() {
   server.send(200, "text/html", html);
 }
 
-/*** 初期化(クライアントモード) ***/
+/**
+ * 初期化(クライアントモード)
+ */
 void setup_client() {
 
   File f = SPIFFS.open(settings, "r");
@@ -66,7 +82,9 @@ void setup_client() {
   Serial.println(WiFi.localIP());
 }
 
-/*** 初期化(サーバモード) ***/
+/**
+ * 初期化(サーバモード)
+ */
 void setup_server() {
   byte mac[6];
   WiFi.macAddress(mac);
@@ -86,19 +104,28 @@ void setup_server() {
   Serial.println("HTTP server started.");
 }
 
-/*** 初期化 ***/
+/**
+ * 初期化
+ */
 void setup() {
   Serial.begin(115200);
   Serial.println();
-  // 2秒以内にMODEを切り替える 0:Server/1:Client
-  delay(2000);
-  SPIFFS.begin();  // ファイルシステム初期化
+
+  // 1秒以内にMODEを切り替える
+  //  0 : Server
+  //  1 : Client
+  delay(1000);
+
+  // ファイルシステム初期化
+  SPIFFS.begin();
 
   pinMode(MODE_PIN, INPUT);
   if (digitalRead(MODE_PIN) == 0) {
-    setup_server();    // サーバモード初期化
+    // サーバモード初期化
+    setup_server();
   } else {
-    setup_client();    // クライアントモード初期化
+    // クライアントモード初期化
+    setup_client();
   }
 }
 
