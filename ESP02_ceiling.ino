f34644a070eed29dff91a0f4d015edd17b5cbf6f
Tadashi Ogasawara <1bjt2225@gmail.com>
2018-01-21 01:03:56 +0900

    merging

--- ESP02_ceiling.ino
+++ ESP02_ceiling.ino
@@ -1,24 +1,35 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+#include <DNSServer.h>
 #include <FS.h>
 
 
-const int MODE_PIN = 0; // GPIO0// モード切り替えピン
-const char* settings = "/wifi_settings.txt";// Wi-Fi設定保存ファイル
-const String pass = "password";// サーバモードでのパスワード
+const int    MODE_PIN = 0; // GPIO0// モード切り替えピン
+const char*  settings = "/wifi_settings.txt";// Wi-Fi設定保存ファイル
+const String pass     = "password";// サーバモードでのパスワード
+const string AP_TITLE = "ESP02_ceiling";
 
+const byte DNS_PORT = 53;
+IPAddress apIP(192, 168, 1, 1);
+DNSServer dnsServer;
 ESP8266WebServer server(80);
+
+
 
 /*** WiFi設定 ***/
 void handleRootGet() {
-  String html = "";
-  html += "<h1>WiFi Settings</h1>";
-  html += "<form method='post'>";
-  html += "  <input type='text' name='ssid' placeholder='ssid'><br>";
-  html += "  <input type='text' name='pass' placeholder='pass'><br>";
-  html += "  <input type='submit'><br>";
-  html += "</form>";
+  String html = ""
+    "<!DOCTYPE html>"
+    "<html><head>"
+    "<title>CaptivePortal</title>"
+    "</head><body>"
+    "<h1>WiFi Settings</h1>"
+    "<form method='post'>"
+    "  <input type='text' name='ssid' placeholder='ssid'><br>"
+    "  <input type='text' name='pass' placeholder='pass'><br>"
+    "  <input type='submit'><br>"
+    "</form>";
   server.send(200, "text/html", html);
 }
 
@@ -32,9 +43,14 @@ void handleRootPost() {
   f.close();
 
   String html = "";
-  html += "<h1>WiFi Settings</h1>";
-  html += ssid + "<br>";
-  html += pass + "<br>";
+    html += "<!DOCTYPE html>"
+    html += "<html><head>"
+    html += "<title>CaptivePortal</title>"
+    html += "</head><body>"
+    html += "<h1>WiFi Settings</h1>";
+    html += ssid + "<br>";
+    html += pass + "<br>";
+    html += "</body></html>";
   server.send(200, "text/html", html);
 }
 
@@ -59,9 +75,7 @@ void setup_client() {
     Serial.print(".");
   }
   Serial.println("");
-
   Serial.println("WiFi connected");
-
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 }
@@ -80,8 +94,13 @@ void setup_server() {
   /* You can remove the password parameter if you want the AP to be open. */
   WiFi.softAP(ssid.c_str(), pass.c_str());
 
-  server.on("/", HTTP_GET, handleRootGet);
-  server.on("/", HTTP_POST, handleRootPost);
+  dnsServer.start(DNS_PORT, "*", apIP);
+
+  server.onNotFound([]() {
+    server.send(200, "text/html", responseHTML);
+    server.on("/", HTTP_GET, handleRootGet);
+    server.on("/", HTTP_POST, handleRootPost);
+  }
   server.begin();
   Serial.println("HTTP server started.");
 }
@@ -90,9 +109,8 @@ void setup_server() {
 void setup() {
   Serial.begin(115200);
   Serial.println();
-  // 2秒以内にMODEを切り替える 0:Server/1:Client
-  delay(2000);
-  SPIFFS.begin();  // ファイルシステム初期化
+  delay(2000);    // 2秒以内にMODEを切り替える 0:Server/1:Client
+  SPIFFS.begin(); // ファイルシステム初期化
 
   pinMode(MODE_PIN, INPUT);
   if (digitalRead(MODE_PIN) == 0) {
@@ -103,5 +121,6 @@ void setup() {
 }
 
 void loop() {
+  dnsServer.processNextRequest();
   server.handleClient();
 }
